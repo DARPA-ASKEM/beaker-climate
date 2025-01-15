c8ce015e7ac70fe432d3b2fde26f6fd3dfc9d02a
Brandon Rose <rose.brandon.m@gmail.com>
2025-01-15 09:33:42 -0600

    remove extraneous tools, bump beaker-kernel versio (#9)

--- src/beaker_climate/beaker_climate/agent.py
+++ src/beaker_climate/beaker_climate/agent.py
@@ -97,109 +97,109 @@ class ClimateDataUtilityAgent(BeakerAgent):
             self.add_context(f"The tool failed with this error: {str(e)}. I should inform the user immediately with the full text of the error.")
             return {}
         
-    @tool()
-    async def regrid_dataset(
-        self,
-        dataset: str,
-        target_resolution: tuple,
-        agent: AgentRef,
-        loop: LoopControllerRef,
-        aggregation: Optional[str] = "interp_or_mean",
-    ) -> str:
-        """
-        This tool should be used to show the user code to regrid a netcdf dataset with detectable geo-resolution.
+    # @tool()
+    # async def regrid_dataset(
+    #     self,
+    #     dataset: str,
+    #     target_resolution: tuple,
+    #     agent: AgentRef,
+    #     loop: LoopControllerRef,
+    #     aggregation: Optional[str] = "interp_or_mean",
+    # ) -> str:
+    #     """
+    #     This tool should be used to show the user code to regrid a netcdf dataset with detectable geo-resolution.
 
-        If a user asks to regrid a dataset, use this tool to return them code to regrid the dataset.
+    #     If a user asks to regrid a dataset, use this tool to return them code to regrid the dataset.
 
-        If you are given a netcdf dataset, use this tool instead of any other regridding tool.
+    #     If you are given a netcdf dataset, use this tool instead of any other regridding tool.
 
-        If you are asked about what is needed to regrid a dataset, please provide information about the arguments of this tool.
+    #     If you are asked about what is needed to regrid a dataset, please provide information about the arguments of this tool.
 
-        Args:
-            dataset (str): The name of the dataset instantiated in the jupyter notebook.
-            target_resolution (tuple): The target resolution to regrid to, e.g. (0.5, 0.5). This is in degrees longitude and latitude.
-            aggregation (Optional): The aggregation function to be used in the regridding. The options are as follows:
-                'conserve'
-                'min'
-                'max'
-                'mean'
-                'median'
-                'mode'
-                'interp_or_mean'
-                'nearest_or_mode'
+    #     Args:
+    #         dataset (str): The name of the dataset instantiated in the jupyter notebook.
+    #         target_resolution (tuple): The target resolution to regrid to, e.g. (0.5, 0.5). This is in degrees longitude and latitude.
+    #         aggregation (Optional): The aggregation function to be used in the regridding. The options are as follows:
+    #             'conserve'
+    #             'min'
+    #             'max'
+    #             'mean'
+    #             'median'
+    #             'mode'
+    #             'interp_or_mean'
+    #             'nearest_or_mode'
 
-        Returns:
-            str: Status of whether or not the dataset has been persisted to the HMI server.
-        """
+    #     Returns:
+    #         str: Status of whether or not the dataset has been persisted to the HMI server.
+    #     """
 
-        loop.set_state(loop.STOP_SUCCESS)
-        code = agent.context.get_code(
-            "flowcast_regridding",
-            {
-                "dataset": dataset,
-                "target_resolution": target_resolution,
-                "aggregation": aggregation,
-            },
-        )
+    #     loop.set_state(loop.STOP_SUCCESS)
+    #     code = agent.context.get_code(
+    #         "flowcast_regridding",
+    #         {
+    #             "dataset": dataset,
+    #             "target_resolution": target_resolution,
+    #             "aggregation": aggregation,
+    #         },
+    #     )
 
-        result = json.dumps(
-            {
-                "action": "code_cell",
-                "language": "python3",
-                "content": code.strip(),
-            }
-        )
+    #     result = json.dumps(
+    #         {
+    #             "action": "code_cell",
+    #             "language": "python3",
+    #             "content": code.strip(),
+    #         }
+    #     )
 
-        return result
+    #     return result
 
-    @tool()
-    async def get_netcdf_plot(
-        self,
-        dataset_variable_name: str,
-        agent: AgentRef,
-        loop: LoopControllerRef,
-        plot_variable_name: Optional[str] = None,
-        lat_col: Optional[str] = "lat",
-        lon_col: Optional[str] = "lon",
-        time_slice_index: Optional[int] = 1,
-    ) -> str:
-        """
-        This function should be used to get a plot of a netcdf dataset.
+    # @tool()
+    # async def get_netcdf_plot(
+    #     self,
+    #     dataset_variable_name: str,
+    #     agent: AgentRef,
+    #     loop: LoopControllerRef,
+    #     plot_variable_name: Optional[str] = None,
+    #     lat_col: Optional[str] = "lat",
+    #     lon_col: Optional[str] = "lon",
+    #     time_slice_index: Optional[int] = 1,
+    # ) -> str:
+    #     """
+    #     This function should be used to get a plot of a netcdf dataset.
 
-        This function should also be used to preview any netcdf dataset.
+    #     This function should also be used to preview any netcdf dataset.
 
-        If the user asks to plot or preview a dataset, use this tool to return plotting code to them.
+    #     If the user asks to plot or preview a dataset, use this tool to return plotting code to them.
 
-        You should also ask if the user wants to specify the optional arguments by telling them what each argument does.
+    #     You should also ask if the user wants to specify the optional arguments by telling them what each argument does.
 
-        Args:
-            dataset_variable_name (str): The name of the dataset instantiated in the jupyter notebook.
-            plot_variable_name (Optional): The name of the variable to plot. Defaults to None.
-                If None is provided, the first variable in the dataset will be plotted.
-            lat_col (Optional): The name of the latitude column. Defaults to 'lat'.
-            lon_col (Optional): The name of the longitude column. Defaults to 'lon'.
-            time_slice_index (Optional): The index of the time slice to visualize. Defaults to 1.
+    #     Args:
+    #         dataset_variable_name (str): The name of the dataset instantiated in the jupyter notebook.
+    #         plot_variable_name (Optional): The name of the variable to plot. Defaults to None.
+    #             If None is provided, the first variable in the dataset will be plotted.
+    #         lat_col (Optional): The name of the latitude column. Defaults to 'lat'.
+    #         lon_col (Optional): The name of the longitude column. Defaults to 'lon'.
+    #         time_slice_index (Optional): The index of the time slice to visualize. Defaults to 1.
 
-        Returns:
-            str: The code used to plot the netcdf.
-        """
+    #     Returns:
+    #         str: The code used to plot the netcdf.
+    #     """
 
-        code = agent.context.get_code(
-            "get_netcdf_plot",
-            {
-                "dataset": dataset_variable_name,
-                "plot_variable_name": plot_variable_name,
-                "lat_col": lat_col,
-                "lon_col": lon_col,
-                "time_slice_index": time_slice_index,
-            },
-        )
+    #     code = agent.context.get_code(
+    #         "get_netcdf_plot",
+    #         {
+    #             "dataset": dataset_variable_name,
+    #             "plot_variable_name": plot_variable_name,
+    #             "lat_col": lat_col,
+    #             "lon_col": lon_col,
+    #             "time_slice_index": time_slice_index,
+    #         },
+    #     )
 
-        result = await agent.context.evaluate(
-            code,
-            parent_header={},
-        )
+    #     result = await agent.context.evaluate(
+    #         code,
+    #         parent_header={},
+    #     )
 
-        output = result.get("return")
+    #     output = result.get("return")
 
-        return output
+    #     return output
